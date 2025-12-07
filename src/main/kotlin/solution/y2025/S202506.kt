b89758dee369319bbe9a22b352a7aac81e66eda6
grobin89 <grobin89@gmail.com>
2025-12-07 23:19:25 +1100

    Day 6 in progress

--- src/main/kotlin/solution/y2025/S202506.kt
+++ src/main/kotlin/solution/y2025/S202506.kt
@@ -1,15 +1,22 @@
 package org.example.solution.y2025
 
 import org.example.core.BasicSolution
+import org.example.core.UnifiedSolution
 
-class S202506 : BasicSolution {
+class S202506 : UnifiedSolution<List<Pair<List<Int>,Char>>> {
     override fun name() = "S202506"
 
-    override fun solve1(data: List<String>): String {
+    override fun solve2(data: List<Pair<List<Int>, Char>>): String {
         TODO("Not yet implemented")
     }
 
-    override fun solve2(data: List<String>): String {
+    override fun solve1(data: List<Pair<List<Int>, Char>>): String {
         TODO("Not yet implemented")
     }
+
+    override fun data(input: List<String>): List<Pair<List<Int>, Char>> {
+        val firstParse = input.dropLast(1).map { it.split(Regex("\\w")).map { it.toInt() } }
+        TODO("Not yet implemented")
+    }
+
 }
